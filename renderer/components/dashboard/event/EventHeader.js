a0547d7b4706b44431fcfe2430718ead1f4ed86d
Michael Peters <michaelpeters5647@gmail.com>
2021-08-12 12:47:20 +0200

    Improve form actions view

--- renderer/components/dashboard/event/EventHeader.js
+++ renderer/components/dashboard/event/EventHeader.js
@@ -23,227 +23,205 @@ export default function EventHeader() {
     {
       label: "Message Sent",
       temp: true,
-      tempDescription: "Temp Variable Name (stores message that was sent)",
+      tempDescription: "Stores the message that was sent",
     },
     {
       label: "On Interval",
       temp: true,
-      tempDescription: "Interval of Time (in seconds)",
+      tempLabel: "Interval of Time (in seconds)",
     },
     { label: "Bot Join Server", temp: false, tempDescription: "" },
     { label: "Bot Leave Server", temp: false, tempDescription: "" },
     {
       label: "Member Join Server",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores member that joined the server)",
+      tempDescription: "Stores the member that joined the server",
     },
     {
       label: "Member Leave Server",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores member that left the server)",
+      tempDescription: "Stores the member that left the server",
     },
     {
       label: "Channel Create",
       temp: true,
-      tempDescription: "Temp Variable Name (stores channel that was created)",
+      tempDescription: "Stores the channel that was created",
     },
     {
       label: "Channel Delete",
       temp: true,
-      tempDescription: "Temp Variable Name (stores channel that was deleted)",
+      tempDescription: "Stores the channel that was deleted",
     },
     {
       label: "Role Create",
       temp: true,
-      tempDescription: "Temp Variable Name (stores role that was created)",
+      tempDescription: "Stores the role that was created",
     },
     {
       label: "Role Delete",
       temp: true,
-      tempDescription: "Temp Variable Name (stores role that was deleted)",
+      tempDescription: "Stores the role that was deleted",
     },
     {
       label: "Member Banned",
       temp: true,
-      tempDescription: "Temp Variable Name (stores member that was banned)",
+      tempDescription: "Stores the member that was banned",
     },
     {
       label: "Member Unbanned",
       temp: true,
-      tempDescription: "Temp Variable Name (stores member that was unbanned)",
+      tempDescription: "Stores the member that was unbanned",
     },
     {
       label: "Voice Channel Create",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores voice channel that was created)",
+      tempDescription: "Stores the voice channel that was created",
     },
     {
       label: "Voice Channel Delete",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores voice channel that was deleted)",
+      tempDescription: "Stores the voice channel that was deleted",
     },
     {
       label: "Emoji Create",
       temp: true,
-      tempDescription: "Temp Variable Name (stores emoji that was created)",
+      tempDescription: "Stores the emoji that was created",
     },
     {
       label: "Emoji Delete",
       temp: true,
-      tempDescription: "Temp Variable Name (stores emoji that was deleted)",
+      tempDescription: "Stores the emoji that was deleted",
     },
     {
       label: "Message Deleted",
       temp: true,
-      tempDescription: "Temp Variable Name (stores message that was deleted)",
+      tempDescription: "Stores the message that was deleted",
     },
     {
       label: "Server Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores server prior to update)",
-      temp2Description: "Temp Variable Name (stores server after update)",
+      temp2Description: "Stores the server after update",
     },
     {
       label: "Member Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores member prior to update)",
-      temp2Description: "Temp Variable Name (stores member after update)",
+      tempDescription: "Stores the member prior to update",
+      temp2Description: "Stores the member after update",
     },
     {
       label: "Presence Update",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores member prior to their presence update)",
-      temp2Description:
-        "Temp Variable Name (stores member after their presence update)",
+      tempDescription: "Stores the member prior to their presence update",
+      temp2Description: "Stores the member after their presence update",
     },
     {
       label: "Member Voice Update",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores member prior to their voice update)",
-      temp2Description:
-        "Temp Variable Name (stores member after their voice update)",
+      tempDescription: "Stores the member prior to their voice update",
+      temp2Description: "Stores the member after their voice update",
     },
     {
       label: "Channel Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores channel prior to update)",
-      temp2Description: "Temp Variable Name (stores channel after update)",
+      tempDescription: "Stores the channel prior to update",
+      temp2Description: "Stores the channel after update",
     },
     {
       label: "Channel Pins Update",
       temp: true,
-      tempDescription: "Temp Variable Name (stores channel prior to update)",
+      tempDescription: "Stores the channel prior to update",
     },
     {
       label: "Role Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores role prior to update)",
-      temp2Description: "Temp Variable Name (stores role after update)",
+      tempDescription: "Stores the role prior to update",
+      temp2Description: "Stores the role after update",
     },
     {
       label: "Message Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores message prior to update)",
-      temp2Description: "Temp Variable Name (stores message after update)",
+      tempDescription: "Stores the message prior to update",
+      temp2Description: "Stores the message after update",
     },
     {
       label: "Emoji Update",
       temp: true,
       temp2: true,
-      tempDescription: "Temp Variable Name (stores emoji prior to update)",
-      temp2Description: "Temp Variable Name (stores emoji after update)",
+      tempDescription: "Stores the emoji prior to update",
+      temp2Description: "Stores the emoji after update",
     },
     {
       label: "Message Reaction Added",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores message reaction that was added)",
+      tempDescription: "Stores the message reaction that was added",
     },
     {
       label: "Message Reaction Removed",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores message reaction that was removed)",
+      tempDescription: "Stores the message reaction that was removed",
     },
     {
       label: "All Message Reactions Removed",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores message that had all reactions removed)",
+      tempDescription: "Stores the message that had all reactions removed",
     },
     {
       label: "Member Becomes Available",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores member that became available)",
+      tempDescription: "Stores the member that became available",
     },
     {
       label: "Member Chunck Received",
       temp: true,
-      tempDescription: "Temp Variable Name (stores members from chunck)",
+      tempDescription: "Stores the members from chunck",
     },
     {
       label: "Member Starts/StopsSpeaking",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores member that started/stopped speaking)",
+      tempDescription: "Stores the member that started/stopped speaking",
       temp2Description:
-        "Temp Variable Name (stores boolean of whether they started/stopped speaking)",
+        "Stores the boolean of whether they started/stopped speaking",
     },
     {
       label: "Member Typing Starts",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores channel where member started typing)",
-      temp2Description:
-        "Temp Variable Name (stores member that started typing)",
+      tempDescription: "Stores the channel where member started typing",
+      temp2Description: "Stores the member that started typing",
     },
     {
       label: "Member Typing Stops",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores channel where member stopped typing)",
-      temp2Description:
-        "Temp Variable Name (stores member that stopped typing)",
+      tempDescription: "Stores the channel where member stopped typing",
+      temp2Description: "Stores the member that stopped typing",
     },
     {
       label: "Server Becomes Unavailable",
       temp: true,
-      tempDescription:
-        "Temp Variable Name (stores server that became unavailable)",
+      tempDescription: "Stores the server that became unavailable",
     },
     {
       label: "On Bot Error",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores Discord Bot Creator error text)",
-      temp2Description:
-        "Temp Variable Name (stores Discord Bot Creator error code)",
+      tempDescription: "Discord Bot Creator error text",
+      temp2Description: "Discord Bot Creator error code",
     },
     {
       label: "On Time Restricted Command",
       temp: true,
       temp2: true,
-      tempDescription:
-        "Temp Variable Name (stores member that ran the command)",
-      temp2Description: "Temp Variable Name (stores amount of time needed)",
+      tempDescription: "Member that ran the command",
+      temp2Description: "Amount of time needed",
     },
   ];
 
@@ -305,12 +283,15 @@ export default function EventHeader() {
           </Form.Group>
           {option?.temp && (
             <Form.Group className="mb-3">
-              <Form.Label>{option.tempDescription}</Form.Label>
+              <Form.Label>
+                {option.tempLabel || "Temp Variable Name"}
+              </Form.Label>
               <FormControl
                 type="text"
                 value={event?.temp}
                 onChange={onChangeTemp}
               />
+              <Form.Text>{option.tempDescription || ""}</Form.Text>
             </Form.Group>
           )}
           {option?.temp2 && (
