3871fb6fa7942d3134fc4775cfc56ad22a8d98bf
Michael Peters <michaelpeters5647@gmail.com>
2021-08-16 07:49:59 +0200

    Add more validators

--- main/helpers/validate-files.js
+++ main/helpers/validate-files.js
@@ -25,3 +25,25 @@ export const validateSettingsFile = (rootFolder) => {
   let settings = readFile([rootFolder, "data", "settings.json"]);
   return !!(settings?.token || settings?.clientId);
 };
+
+export const validatePlayersFile = (rootFolder) => {
+  let players = readFile([rootFolder, "data", "players.json"]);
+  return !!Object.keys(players).length;
+};
+
+export const validateServersFile = (rootFolder) => {
+  let servers = readFile([rootFolder, "data", "servers.json"]);
+  return !!Object.keys(servers).length;
+};
+
+export const validateCommandsFile = (rootFolder) => {
+  let commands = readFile([rootFolder, "data", "commands.json"]);
+  return !!Object.keys(commands).length;
+}
+
+export const validateEventsFile = (rootFolder) => {
+  let events = readFile([rootFolder, "data", "events.json"]);
+  return !!Object.keys(events).length;
+}
+
+export const validate
