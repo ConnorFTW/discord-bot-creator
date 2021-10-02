3efe59b672c708ff6cf0bb7e4663052137f5d2de
Michael Peters <michaelpeters5647@gmail.com>
2021-10-02 07:47:27 +0200

    Clean up file

--- resources/actions/add_item_to_list.js
+++ resources/actions/add_item_to_list.js
@@ -1,60 +1,14 @@
 module.exports = {
-  //---------------------------------------------------------------------
-  // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  //---------------------------------------------------------------------
-
   name: "Add Item to List",
-
-  //---------------------------------------------------------------------
-  // Action Section
-  //
-  // This is the section the action will fall into.
-  //---------------------------------------------------------------------
-
   section: "Lists and Loops",
-
-  //---------------------------------------------------------------------
-  // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  //---------------------------------------------------------------------
-
-  subtitle: function (data) {
+  subtitle(data) {
     const storage = ["", "Temp Variable", "Server Variable", "Global Variable"];
     return `Add "${data.value}" to ${storage[parseInt(data.storage)]} (${
       data.varName
     })`;
   },
-
-  //---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
-
   fields: ["storage", "varName", "addType", "position", "value"],
-
-  //---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editting actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //
-  // The "data" parameter stores constants for select elements to use.
-  // Each is an array: index 0 for commands, index 1 for events.
-  // The names are: sendTargets, members, roles, channels,
-  //                messages, servers, variables
-  //---------------------------------------------------------------------
-
-  html: function (isEvent, data) {
+  html(isEvent, data) {
     return `
 <div>
 	<div style="float: left; width: 35%;">
@@ -87,15 +41,6 @@ module.exports = {
 	<input id="value" class="round" type="text" name="is-eval">
 </div>`;
   },
-
-  //---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  //---------------------------------------------------------------------
-
   init: function () {
     const { glob, document } = this;
 
@@ -113,15 +58,7 @@ module.exports = {
     glob.onChange1(document.getElementById("addType"));
   },
 
-  //---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existance.
-  //---------------------------------------------------------------------
-
-  action: function (cache) {
+  action(cache) {
     const data = cache.actions[cache.index];
     const storage = parseInt(data.storage);
     const varName = this.evalMessage(data.varName, cache);
@@ -156,15 +93,4 @@ module.exports = {
 
     this.callNextAction(cache);
   },
-
-  //---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflictions between mods, be sure to alias
-  // functions you wish to overwrite.
-  //---------------------------------------------------------------------
-
-  mod: function () {},
 }; // End of module
