3af4ac4ed0384adec7ae22e1e417a381f6a3f755
Michael Peters <michaelpeters5647@gmail.com>
2021-10-04 14:24:40 +0200

    Use simple editor instead

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,31 +1,12 @@
 const path = require("path");
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const withTM = require("next-transpile-modules")(["monaco-editor"]);
 
 module.exports = withTM({
   webpack: (config, { isServer }) => {
-    const rule = config.module.rules
-      .find((rule) => rule.oneOf)
-      .oneOf.find(
-        (r) => r.issuer && r.issuer.include && r.issuer.include.includes("_app")
-      );
-    if (rule) {
-      rule.issuer.include = [
-        rule.issuer.include,
-        /[\\/]node_modules[\\/]monaco-editor[\\/]/,
-      ];
-    }
-
     if (!isServer) {
       config.target = "electron-renderer";
     }
     config.node.__dirname = true;
-    config.plugins.push(
-      new MonacoWebpackPlugin({
-        languages: ["javascript"],
-        filename: "static/[name].worker.js",
-      })
-    );
 
     return config;
   },
