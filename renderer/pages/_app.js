242da7c3ebc92256fc0a610113cde77d7311e6d2
Michael55555 <michaelpeters5647@gmail.com>
2021-10-05 10:54:04 +0200

    Remove breaking log

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -1,10 +1,5 @@
-import fs from "fs";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import "../styles/style.scss";
-
-// list all files in the renderer folder
-const files = fs.readdirSync("./renderer");
-console.log({ files });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
